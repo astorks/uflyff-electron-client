29e94725d6695ed2fd66815c85f039fd15ab1617
Ashton Storks <ashton@pixlfox.com>
2022-07-01 22:58:34 -0600

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,190 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain, globalShortcut } from "electron";
 import * as path from "path";
+
+
 
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    height: 600,
+  const wnd = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    icon: path.join(__dirname, "../img/favicon.png"),
+    titleBarStyle: "hidden",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      webviewTag: true,
     },
-    width: 800,
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  wnd.loadFile(path.join(__dirname, "../index.html"));
+//   wnd.webContents.openDevTools();
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  return wnd;
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+let flyffipediaWindow: BrowserWindow = null;
+function windowShowFlyffipedia() {
+    if(flyffipediaWindow && !flyffipediaWindow.isDestroyed()) {
+        flyffipediaWindow.show();
+        return;
+    }
+
+    // Create the browser window.
+    flyffipediaWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: path.join(__dirname, "../img/favicon-flyffipedia.png"),
+        titleBarStyle: "hidden",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            webviewTag: true,
+        },
+    });
+
+    flyffipediaWindow.loadFile(path.join(__dirname, "../flyffipedia.html"));
+}
+
+
+let madrigalinsideWindow: BrowserWindow = null;
+function windowShowMadrigalinside() {
+    if(madrigalinsideWindow && !madrigalinsideWindow.isDestroyed()) {
+        madrigalinsideWindow.show();
+        return;
+    }
+
+    // Create the browser window.
+    madrigalinsideWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: path.join(__dirname, "../img/favicon-madrigalinside.png"),
+        titleBarStyle: "hidden",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            webviewTag: true,
+        },
+    });
+
+    madrigalinsideWindow.loadFile(path.join(__dirname, "../madrigalinside.html"));
+}
+
+let flyffulatorWindow: BrowserWindow = null;
+function windowShowFlyffulator() {
+    if(flyffulatorWindow && !flyffulatorWindow.isDestroyed()) {
+        flyffulatorWindow.show();
+        return;
+    }
+
+    // Create the browser window.
+    flyffulatorWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: path.join(__dirname, "../img/favicon-flyffulator.png"),
+        titleBarStyle: "hidden",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            webviewTag: true,
+        },
+    });
+
+    flyffulatorWindow.loadFile(path.join(__dirname, "../flyffulator.html"));
+}
+
+let madrigalmapsWindow: BrowserWindow = null;
+function windowShowMadrigalmaps() {
+    if(madrigalmapsWindow && !madrigalmapsWindow.isDestroyed()) {
+        madrigalmapsWindow.show();
+        return;
+    }
+
+    // Create the browser window.
+    madrigalmapsWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: path.join(__dirname, "../img/favicon-madrigalmaps.png"),
+        titleBarStyle: "hidden",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            webviewTag: true,
+        },
+    });
+
+    madrigalmapsWindow.loadFile(path.join(__dirname, "../madrigalmaps.html"));
+}
+
+let modelviewerWindow: BrowserWindow = null;
+function windowShowModelviewer() {
+    if(modelviewerWindow && !modelviewerWindow.isDestroyed()) {
+        modelviewerWindow.show();
+        return;
+    }
+
+    // Create the browser window.
+    modelviewerWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        icon: path.join(__dirname, "../img/favicon-modelviewer.png"),
+        titleBarStyle: "hidden",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            webviewTag: true,
+        },
+    });
+
+    modelviewerWindow.loadFile(path.join(__dirname, "../modelviewer.html"));
+}
+
+
+
+
+ipcMain.on('window-close', (event) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.close();
+});
+
+ipcMain.on('window-minimize', (event) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.minimize();
+});
+
+ipcMain.on('window-fullscreen', (event) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.setFullScreen(!win.isFullScreen());
+    return win.isFullScreen();
+});
+
+ipcMain.on('window-maximize', (event) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    if(win.isMaximized()) {
+        win.unmaximize();
+    } else {
+        win.maximize();
+    }
+});
+
+ipcMain.on('window-show-flyffipedia', () => windowShowFlyffipedia());
+ipcMain.on('window-show-madrigalinside', () => windowShowMadrigalinside());
+ipcMain.on('window-show-flyffulator', () => windowShowFlyffulator());
+ipcMain.on('window-show-madrigalmaps', () => windowShowMadrigalmaps());
+ipcMain.on('window-show-modelviewer', () => windowShowModelviewer());
+ipcMain.on('window-create', () => createWindow());
+
 app.on("ready", () => {
-  createWindow();
+    createWindow();
 
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+    // globalShortcut.register('CommandOrControl+1', () => mainWindow.webContents.send("game-sendKeypress", "1"));
+    // globalShortcut.register('CommandOrControl+2', () => mainWindow.webContents.send("game-sendKeypress", "2"));
+    // globalShortcut.register('CommandOrControl+3', () => mainWindow.webContents.send("game-sendKeypress", "3"));
+    // globalShortcut.register('CommandOrControl+4', () => mainWindow.webContents.send("game-sendKeypress", "4"));
+    // globalShortcut.register('CommandOrControl+5', () => mainWindow.webContents.send("game-sendKeypress", "5"));
+    // globalShortcut.register('CommandOrControl+6', () => mainWindow.webContents.send("game-sendKeypress", "6"));
+    // globalShortcut.register('CommandOrControl+7', () => mainWindow.webContents.send("game-sendKeypress", "7"));
+    // globalShortcut.register('CommandOrControl+8', () => mainWindow.webContents.send("game-sendKeypress", "8"));
+    // globalShortcut.register('CommandOrControl+9', () => mainWindow.webContents.send("game-sendKeypress", "9"));
+    // globalShortcut.register('CommandOrControl+0', () => mainWindow.webContents.send("game-sendKeypress", "0"));
 });
+app.on("window-all-closed", () => app.quit());
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- src/preload.ts
+++ src/preload.ts
@@ -1,5 +1,5 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+import { contextBridge, ipcRenderer } from 'electron';
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
@@ -12,3 +12,21 @@ window.addEventListener("DOMContentLoaded", () => {
     replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
   }
 });
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  onGameSendKeypress: (callback: any) => ipcRenderer.on('game-sendKeypress', callback),
+  onGameCopy: (callback: any) => ipcRenderer.on('game-copy', callback),
+  onGamePaste: (callback: any) => ipcRenderer.on('game-paste', callback),
+  onGameLogin: (callback: any) => ipcRenderer.on('game-login', callback),
+
+  windowClose: () => ipcRenderer.send('window-close'),
+  windowMinimize: () => ipcRenderer.send('window-minimize'),
+  windowMaximize: () => ipcRenderer.send('window-maximize'),
+  windowFullscreen: () => ipcRenderer.send('window-fullscreen'),
+  windowShowFlyffipedia: () => ipcRenderer.send('window-show-flyffipedia'),
+  windowShowMadrigalinside: () => ipcRenderer.send('window-show-madrigalinside'),
+  windowShowFlyffulator: () => ipcRenderer.send('window-show-flyffulator'),
+  windowShowMadrigalmaps: () => ipcRenderer.send('window-show-madrigalmaps'),
+  windowShowModelviewer: () => ipcRenderer.send('window-show-modelviewer'),
+  windowCreate: () => ipcRenderer.send('window-create'),
+})
